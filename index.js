61ecce57e243c800087fb9c77f6fe07566f251b0
David Ma <david.ma@petermac.org>
2017-03-18 16:29:03 +1100

    Initial Commit - Basic node-unblocker from nfriedly

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var Unblocker = require('unblocker');
+
+
+var unblocker = Unblocker({});
+
+http.createServer(function(req, res) {
+  console.log("new request..?");
+
+    // first let unblocker try to handle the requests
+    unblocker(req, res, function(err) {
+
+        console.log("hey...");
+
+        // this callback will be fired for any request that unblocker does not serve
+        var headers = {'content-type': 'text/plain'};
+        if (err) {
+            res.writeHead(500, headers);
+            return res.end(err.stack || err.message);
+        }
+        if (req.url == '/') {
+            res.writeHead(200, headers);
+            return res.end('Use the format http://thissite.com/proxy/http://site-i-want.com/ to access the proxy.');
+        } else {
+            res.writeHead(404, headers);
+            return res.end('Error 404: file not found.');
+        }
+    });
+}).listen(8080);
