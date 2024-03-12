6f764fcac189b4c663954012a1a3fa93c0041745
David Ma <hello@david-ma.net>
2024-03-12 13:51:57 +1100

    Add google analytics and set a cookie to stop bots

--- config/config.js
+++ config/config.js
@@ -2,20 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 const unblocker = require('unblocker');
+var Transform = require('stream').Transform;
 var unblockerConfig = {
     prefix: '/proxy/',
-    requestMiddleware: [],
-    responseMiddleware: [],
+    responseMiddleware: [googleAnalyticsMiddleware],
 };
 let config = {
     controllers: {
+        '': function (controller) {
+            controller.res.setCookie({ cookieName: controller.ip });
+            if (controller.query.goto) {
+                controller.response.writeHead(302, {
+                    Location: controller.query.goto,
+                });
+                controller.response.end();
+                return;
+            }
+            else {
+                controller.routeFile(`${__dirname}/../public/index.html`);
+            }
+        },
         proxy: function (controller) {
             const url = controller.request.url;
             const sections = url.split('/proxy/');
             controller.request.url = `${sections[0]}/proxy/${sections.pop()}`;
+            const cookies = controller.cookies;
             if (sections.length > 3) {
                 controller.response.writeHead(302, {
                     Location: controller.request.url,
+                });
+                controller.response.end();
+                return;
+            }
+            else if (!cookies ||
+                !cookies.cookieName ||
+                cookies.cookieName !== controller.ip) {
+                controller.response.writeHead(303, {
+                    Location: `//${controller.request.headers.host}/?goto=${url}`,
                 });
                 controller.response.end();
                 return;
@@ -42,3 +65,33 @@ let config = {
     },
 };
 exports.config = config;
+var google_analytics_id = process.env.GA_ID || 'UA-49861162-2';
+function addGa(html) {
+    if (google_analytics_id) {
+        var ga = [
+            '<script type="text/javascript">',
+            'var _gaq = []; // overwrite the existing one, if any',
+            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
+            "_gaq.push(['_trackPageview']);",
+            '(function() {',
+            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
+            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
+            '})();',
+            '</script>',
+        ].join('\n');
+        html = html.replace('</body>', ga + '\n\n</body>');
+    }
+    return html;
+}
+function googleAnalyticsMiddleware(data) {
+    if (data.contentType == 'text/html') {
+        data.stream = data.stream.pipe(new Transform({
+            decodeStrings: false,
+            transform: function (chunk, encoding, next) {
+                this.push(addGa(chunk.toString()));
+                next();
+            },
+        }));
+    }
+}
--- config/config.ts
+++ config/config.ts
@@ -1,26 +1,47 @@
 const unblocker = require('unblocker')
 import { Thalia } from 'thalia'
+var Transform = require('stream').Transform
 
 var unblockerConfig = {
   prefix: '/proxy/',
-  requestMiddleware: [
-    // cookieChecker
-  ],
-  responseMiddleware: [
-    // googleAnalyticsMiddleware
-  ],
+  responseMiddleware: [googleAnalyticsMiddleware],
 }
 
 let config: Thalia.WebsiteConfig = {
   controllers: {
+    '': function (controller) {
+      controller.res.setCookie({ cookieName: controller.ip })
+
+      if (controller.query.goto) {
+        controller.response.writeHead(302, {
+          Location: controller.query.goto,
+        })
+        controller.response.end()
+        return
+      } else {
+        controller.routeFile(`${__dirname}/../public/index.html`)
+      }
+    },
     proxy: function (controller) {
       const url = controller.request.url
       const sections = url.split('/proxy/')
       controller.request.url = `${sections[0]}/proxy/${sections.pop()}`
 
+      const cookies = controller.cookies
+
       if (sections.length > 3) {
         controller.response.writeHead(302, {
           Location: controller.request.url,
+        })
+        controller.response.end()
+        return
+      } else if (
+        !cookies ||
+        !cookies.cookieName ||
+        cookies.cookieName !== controller.ip
+      ) {
+        controller.response.writeHead(303, {
+          Location: `//${controller.request.headers.host}/?goto=${url}`,
         })
         controller.response.end()
         return
@@ -52,3 +73,39 @@ let config: Thalia.WebsiteConfig = {
 }
 
 export { config }
+
+var google_analytics_id = process.env.GA_ID || 'UA-49861162-2'
+
+function addGa(html) {
+  if (google_analytics_id) {
+    var ga = [
+      '<script type="text/javascript">',
+      'var _gaq = []; // overwrite the existing one, if any',
+      "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
+      "_gaq.push(['_trackPageview']);",
+      '(function() {',
+      "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
+      "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+      "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
+      '})();',
+      '</script>',
+    ].join('\n')
+    html = html.replace('</body>', ga + '\n\n</body>')
+  }
+  return html
+}
+
+function googleAnalyticsMiddleware(data) {
+  if (data.contentType == 'text/html') {
+    // https://nodejs.org/api/stream.html#stream_transform
+    data.stream = data.stream.pipe(
+      new Transform({
+        decodeStrings: false,
+        transform: function (chunk, encoding, next) {
+          this.push(addGa(chunk.toString()))
+          next()
+        },
+      })
+    )
+  }
+}
