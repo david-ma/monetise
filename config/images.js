9988a66bdd25d3ad1b334f4652f231229d0c556b
David Ma <hello@david-ma.net>
2024-04-09 13:57:46 +1000

    Work on uploading paintings

--- config/images.js
+++ config/images.js
@@ -1,5 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
 const fs = require('fs');
 const Painting = require('./db_bootstrap').seq.Painting;
 fs.readdir(`${__dirname}/../data/Monet`, (err, files) => {
@@ -33,8 +37,63 @@ fs.readdir(`${__dirname}/../data/Monet`, (err, files) => {
                 filename,
             },
         }).then((painting) => painting[0]);
-    })).then(uploadPaintings);
+    }))
+        .then(viewPaintings);
 });
+function fixFilenames(paintings) {
+    paintings.forEach((painting) => {
+        const newFilename = painting.filename
+            .replace('_.jpg', '.jpg')
+            .replace(/É/g, 'E')
+            .replace(/[éè]/g, 'e')
+            .replace('ç', 'c')
+            .replace('Î', 'I');
+        painting.update({
+            filename: newFilename,
+        });
+    });
+    return paintings;
+}
+function viewPaintings(paintings) {
+    console.log('Paintings', paintings.length);
+    paintings.forEach((painting) => {
+        http_1.default.get('http://localhost:1337/paintings/' + painting.filename, (res) => {
+            if (res.statusCode === 404) {
+                console.log('404', painting.filename);
+            }
+        });
+    });
+    return paintings;
+}
+function checkPaintings(paintings) {
+    paintings.forEach((painting) => {
+        if (painting.title.match(/[^a-zA-Z0-9ÉéèçÎ-\s]/)) {
+            console.log('Weird characters in title', painting.title);
+        }
+    });
+}
+function copyPaintings(paintings) {
+    paintings.forEach((painting) => {
+        const newFilename = painting.filename
+            .replace(/É/g, 'E')
+            .replace(/[éè]/g, 'e')
+            .replace(/ç/g, 'c')
+            .replace(/Î/g, 'I')
+            .replace(/[^a-zA-Z0-9-]/g, '_')
+            .replace(/_+/g, '_')
+            .replace('_jpg', '.jpg')
+            .replace('_.jpg', '.jpg');
+        fs.copyFile(`${__dirname}/../data/Monet/${painting.filename}`, `${__dirname}/../data/paintings/${newFilename}`, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            painting.update({
+                filename: newFilename,
+            });
+        });
+    });
+}
 function uploadPaintings(paintings) {
     console.log('Ok, now do upload the paintings somewhere', paintings.length);
     paintings.forEach((painting) => {
--- config/images.ts
+++ config/images.ts
@@ -1,6 +1,7 @@
 // PBLZL2
 
 import { PaintingModel, PaintingStatic } from '../models'
+import http from 'http'
 
 // fs read files in directory data/Monet
 
@@ -8,7 +9,13 @@ const fs = require('fs')
 const Painting: PaintingStatic = require('./db_bootstrap').seq.Painting
 
 // Drop table painting
-// Painting.sync({ force: true })
+// Painting.sync({ 
+//   alter: true,
+//   force: true
+// })
+
+// console.log("this should drop paintings")
+// process.exit()
 
 fs.readdir(`${__dirname}/../data/Monet`, (err, files) => {
   if (err) {
@@ -51,8 +58,80 @@ fs.readdir(`${__dirname}/../data/Monet`, (err, files) => {
         },
       }).then((painting) => painting[0])
     })
-  ).then(uploadPaintings)
+  )
+  // .then(copyPaintings)
+  .then(viewPaintings)
 })
+
+function fixFilenames(paintings: PaintingModel[]) {
+  paintings.forEach((painting) => {
+    const newFilename = painting.filename
+      .replace('_.jpg', '.jpg')
+      .replace(/É/g, 'E')
+      .replace(/[éè]/g, 'e')
+      .replace('ç', 'c')
+      .replace('Î', 'I')
+
+    painting.update({
+      filename: newFilename,
+    })
+  })
+  return paintings
+}
+
+function viewPaintings(paintings: PaintingModel[]) {
+  console.log('Paintings', paintings.length)
+  paintings.forEach((painting) => {
+
+    // Check we don't have 404 at localhost:1337/paintings/filename
+    http.get(
+      'http://localhost:1337/paintings/' + painting.filename,
+      (res) => {
+        if (res.statusCode === 404) {
+          console.log('404', painting.filename)
+        }
+      }
+    )
+  })
+  return paintings
+}
+
+function checkPaintings(paintings: PaintingModel[]) {
+  paintings.forEach((painting) => {
+    // Find weird chaaracters:
+    if (painting.title.match(/[^a-zA-Z0-9ÉéèçÎ-\s]/)) {
+      console.log('Weird characters in title', painting.title)
+    }
+  })
+}
+
+function copyPaintings(paintings: PaintingModel[]) {
+  paintings.forEach((painting) => {
+    const newFilename = painting.filename
+      .replace(/É/g, 'E')
+      .replace(/[éè]/g, 'e')
+      .replace(/ç/g, 'c')
+      .replace(/Î/g, 'I')
+      .replace(/[^a-zA-Z0-9-]/g, '_')
+      .replace(/_+/g, '_')
+      .replace('_jpg', '.jpg')
+      .replace('_.jpg', '.jpg')
+
+      fs.copyFile(
+      `${__dirname}/../data/Monet/${painting.filename}`,
+      `${__dirname}/../data/paintings/${newFilename}`,
+      (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        painting.update({
+          filename: newFilename,
+        })
+      }
+    )
+  })
+}
 
 function uploadPaintings(paintings: PaintingModel[]) {
   console.log('Ok, now do upload the paintings somewhere', paintings.length)
